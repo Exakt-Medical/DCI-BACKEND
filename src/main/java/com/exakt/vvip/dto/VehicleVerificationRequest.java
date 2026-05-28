8bf3372e17104cc2bc684264df59354940216be5
nicole <garquiza@exakt.com.ph>
2026-05-29 00:34:22 +0800

    updated logic of verification saving

--- src/main/java/com/exakt/vvip/dto/VehicleVerificationRequest.java
+++ src/main/java/com/exakt/vvip/dto/VehicleVerificationRequest.java
@@ -7,11 +7,20 @@ import lombok.Setter;
 @Getter
 @Setter
 public class VehicleVerificationRequest {
+
     private String mvFileNumber;
-
     private String plateNumber;
-
     private String engineNumber;
-
     private String chassisNumber;
+
+    private String insuranceCode;
+    private String policyNumber;
+    private String premiumType;
+    private String prescribedPremiumFee;
+    private String dst;
+    private String vat;
+    private String lgt;
+    private String validationFee;
+    private String totalAmount;
+    private String voucherCode;
 }
