8d5436dac6e261d1315bcc9962108d0cd041bed4
Angelo Roman Bagasbas <abagasbas@exakt.com.ph>
2026-06-19 17:09:53 +0800

    updated the billeroo webhook route

--- src/main/java/com/dci/clearance/generateVoucher/controller/BillerooWebhookController.java
+++ src/main/java/com/dci/clearance/generateVoucher/controller/BillerooWebhookController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/billeroo")
+@RequestMapping("/api/billeroo")
 @RequiredArgsConstructor
 public class BillerooWebhookController {
 
